3c0c52f2e83621a769963f113200417c2c0bae51
Samuel Abera <sami21.good.bad@gmail.com>
2026-04-04 15:46:47 +0300

    Make footer global

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Space_Grotesk, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import GlobalFooter from "@/components/GlobalFooter";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 const headingFont = Space_Grotesk({
@@ -47,6 +48,7 @@ export default function RootLayout({
         <main className="w-full flex-1 px-0 pb-0 pt-16">
           {children}
         </main>
+        <GlobalFooter />
         <ScrollToTopButton />
       </body>
     </html>
